487c2a155474bb35f27e1212a1e6e20038c61ee3
FrancoJFerrante <francoferrante95@gmail.com>
2020-10-05 11:18:08 -0300

    Initial commit

---
+++ VisualizeGraph/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace VisualizeGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnAgregarConexion = new System.Windows.Forms.Button();
+            this.cmbHasta = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbDesde = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreNodo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNombreConexion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numPesoConexion = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPesoConexion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.numPesoConexion);
+            this.splitContainer1.Panel1.Controls.Add(this.label7);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNombreConexion);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAgregarConexion);
+            this.splitContainer1.Panel1.Controls.Add(this.cmbHasta);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.cmbDesde);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAgregar);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNombreNodo);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 392;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnAgregarConexion
+            // 
+            this.btnAgregarConexion.Location = new System.Drawing.Point(267, 308);
+            this.btnAgregarConexion.Name = "btnAgregarConexion";
+            this.btnAgregarConexion.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarConexion.TabIndex = 10;
+            this.btnAgregarConexion.Text = "Agregar Nodo";
+            this.btnAgregarConexion.UseVisualStyleBackColor = true;
+            this.btnAgregarConexion.Click += new System.EventHandler(this.btnAgregarConexion_Click);
+            // 
+            // cmbHasta
+            // 
+            this.cmbHasta.FormattingEnabled = true;
+            this.cmbHasta.Location = new System.Drawing.Point(256, 250);
+            this.cmbHasta.Name = "cmbHasta";
+            this.cmbHasta.Size = new System.Drawing.Size(121, 21);
+            this.cmbHasta.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Hasta:";
+            // 
+            // cmbDesde
+            // 
+            this.cmbDesde.FormattingEnabled = true;
+            this.cmbDesde.Location = new System.Drawing.Point(256, 224);
+            this.cmbDesde.Name = "cmbDesde";
+            this.cmbDesde.Size = new System.Drawing.Size(121, 21);
+            this.cmbDesde.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Desde:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(65, 308);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar Nodo";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nueva Conexion:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtNombreNodo
+            // 
+            this.txtNombreNodo.Location = new System.Drawing.Point(65, 198);
+            this.txtNombreNodo.Name = "txtNombreNodo";
+            this.txtNombreNodo.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreNodo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nuevo Nodo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(197, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Nombre:";
+            // 
+            // txtNombreConexion
+            // 
+            this.txtNombreConexion.Location = new System.Drawing.Point(256, 198);
+            this.txtNombreConexion.Name = "txtNombreConexion";
+            this.txtNombreConexion.Size = new System.Drawing.Size(121, 20);
+            this.txtNombreConexion.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(203, 280);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Peso:";
+            // 
+            // numPesoConexion
+            // 
+            this.numPesoConexion.Location = new System.Drawing.Point(257, 277);
+            this.numPesoConexion.Name = "numPesoConexion";
+            this.numPesoConexion.Size = new System.Drawing.Size(120, 20);
+            this.numPesoConexion.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numPesoConexion)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreNodo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnAgregarConexion;
+        private System.Windows.Forms.ComboBox cmbHasta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbDesde;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNombreConexion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numPesoConexion;
+    }
+}
+
---
+++ VisualizeGraph/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace VisualizeGraph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Microsoft.Msagl.GraphViewerGdi.GViewer viewer;
+        Microsoft.Msagl.Drawing.Graph graph;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // creo el objeto viewer
+            viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            // creo el objeto graph 
+            graph = new Microsoft.Msagl.Drawing.Graph("graph");
+
+            // Asocio el grafo al viewer
+            viewer.Graph = graph;
+
+            // Asocio el viewer con el splitContainer 
+            splitContainer1.Panel2.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(viewer);
+            splitContainer1.Panel2.ResumeLayout();
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Función que dispara al clickearse el btnAgregar. Agrega un nodo al graph
+            // Si un nodo con el mismo nombre no fue agregado anteriormente, lo agrego a mis comboBoxs.
+            Microsoft.Msagl.Drawing.Node nodoAux = graph.Nodes.FirstOrDefault(q => q.Id == txtNombreNodo.Text);
+            if (nodoAux == null)
+            {
+                graph.AddNode(txtNombreNodo.Text).Attr.Id = txtNombreNodo.Text; // Agrego un nuevo nodo y se le agrega el id que está en el txtNombreNodo
+                graph.FindNode(txtNombreNodo.Text).Attr.Color = Microsoft.Msagl.Drawing.Color.Blue; // Le pongo color azul
+                viewer.Graph = graph;
+                splitContainer1.Panel2.SuspendLayout();
+                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                splitContainer1.Panel2.Controls.Add(viewer);
+                splitContainer1.Panel2.ResumeLayout();
+                cmbDesde.Items.Add(txtNombreNodo.Text); // Agrego el nuevo nodo a los comboBox
+                cmbHasta.Items.Add(txtNombreNodo.Text);
+            }
+
+
+
+        }
+
+        private void btnAgregarConexion_Click(object sender, EventArgs e)
+        {
+            // Agrego un borde con su Id
+            graph.AddEdge(cmbDesde.SelectedItem.ToString(), cmbHasta.SelectedItem.ToString()).Attr.Id = txtNombreConexion.Text;
+            //y le seteo la punta de la flecha muy chiquita para que simule un grafo no direccional
+            graph.EdgeById(txtNombreConexion.Text).Attr.ArrowheadLength = 0.1;
+            // Le asigno un peso
+            graph.EdgeById(txtNombreConexion.Text).Attr.Weight = Convert.ToInt32(numPesoConexion.Value);
+
+            // Actualizo el grafico
+            viewer.Graph = graph;
+            splitContainer1.Panel2.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(viewer);
+            splitContainer1.Panel2.ResumeLayout();
+        }
+    }
+}
+
